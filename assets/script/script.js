dde1da305c6ad8a7ef260cbfe172e88c9dd374ff
AlexGay929 <125366984+AlexGay929@users.noreply.github.com>
2023-03-30 17:06:51 +0800

    first commit

---
+++ assets/script/script.js
@@ -0,0 +1,15 @@
+
+
+
+
+
+
+
+function LengthConverter(valNum) {
+    document.getElementById("outputFeet").innerHTML=valNum*3.2808;
+    document.getElementById("outputInches").innerHTML=valNum*39.370;
+    document.getElementById("outputcm").innerHTML=valNum/0.01;
+  }
+
+
+
